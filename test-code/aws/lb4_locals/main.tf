8103ea29c9676d6766364c4208defa0d6517579a
Saad El-Kenawy <Saad_elkenawy@yahoo.com>
2025-08-24 02:00:11 +0300

    add lb4 locals

---
+++ test-code/aws/lb4_locals/main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# create a vpc 
+resource "aws_vpc" "tf_vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = var.dns_support
+  enable_dns_hostnames = var.dns_hostname
+
+  tags = {
+    Name = "tf_vpc"
+  }
+}
+resource "aws_subnet" "pub_sub" {
+  vpc_id = aws_vpc.tf_vpc.id
+  cidr_block = var.pub_sub_cidr
+  availability_zone = var.Az[0]
+  map_public_ip_on_launch = var.public_ip_on_launch
+
+  tags = {
+    Name = "tf_public_subnet"
+  }
+}
+
+
+resource "aws_subnet" "pri_sub" {
+  vpc_id = aws_vpc.tf_vpc.id
+  cidr_block = var.pri_sub_cidr
+  availability_zone = var.Az[0]
+
+  tags = {
+    Name = "tf_private_subnet"
+  }
+}
+
+
+
+
+  resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.tf_vpc.id
+    tags = {
+      Name = "tf_igw"
+    }
+
+  }
---
+++ test-code/aws/lb4_locals/variables.tf
@@ -0,0 +1,55 @@
+variable "aws_region" {
+  description = "default region is us-east-1"
+  type = string
+  default = "us-east-1"
+}
+
+variable "regions" {
+  type    = list(string)
+  default = ["us-east-1", "us-west-2"]
+}
+
+variable "vpc_cidr" {
+  description = "user input for cidr applied new resource"
+  type = string
+  default = "10.0.0.0/16"
+
+
+}
+
+variable "pub_sub_cidr" {
+  description = "user input for public subnet cidr applied new resource"
+  type = string
+  default = "10.0.100.0/24"
+
+}
+
+variable "pri_sub_cidr" {
+  description = "user input for private subnet cidr applied new resource"
+  type = string
+  default = "10.0.1.0/24"
+
+}
+variable "dns_support" {
+    description = "select dns support true/false"
+    type = bool
+    default = false
+}
+
+variable "dns_hostname" {
+    description = "select dns hostname true/false"
+    type = bool
+    default = false
+}
+
+variable "Az" {
+    description = "availability zone List : us-east-1a, us-east-1b, us-east-1c, us-east-1d, us-east-1e , us-east-1f"
+    type        = list(string)
+    default = [ "us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e" , "us-east-1f" ]
+}
+
+variable "public_ip_on_launch" {
+  description = "Allocate a public IP address on instance launch"
+  type        = bool
+  default = false
+}
