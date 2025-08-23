ffe156774d3d33c89fe22a301c8177ad3aeae477
Saad El-Kenawy <Saad_elkenawy@yahoo.com>
2025-08-24 01:08:49 +0300

    plan ready to go and validated

--- test-code/aws/lb3_variables/main.tf
+++ test-code/aws/lb3_variables/main.tf
@@ -3,16 +3,66 @@ provider "aws" {
 }
 
 # create a vpc 
-resource "aws_vpc" "tf-vpc" {
-  cidr_block = var.cider
+resource "aws_vpc" "tf_vpc" {
+  cidr_block = var.cidr
+  enable_dns_support = var.dns_support
+  enable_dns_hostnames = var.dns_hostname
 
   tags = {
-    Name = "tf-vpc"
+    Name = "tf_vpc"
   }
+}
+resource "aws_subnet" "pub_sub" {
+  vpc_id = aws_vpc.tf_vpc.id
+  cidr_block = var.pub_sub_cidr
+  availability_zone = var.Az[0]
+  map_public_ip_on_launch = var.public_ip_on_launch
 
-
+  tags = {
+    Name = "tf_public_subnet"
+  }
 }
 
-resource "aws_subnet" "name" {
+
+resource "aws_subnet" "pub_sub_2" {
+  vpc_id = aws_vpc.tf_vpc.id
+  cidr_block = var.pub_sub_cidr
+  availability_zone = var.Az[1]
+  map_public_ip_on_launch = var.public_ip_on_launch
+
+  tags = {
+    Name = "tf_public_subnet_2"
+  }
+}
+
+
+resource "aws_subnet" "pri_sub" {
+  vpc_id = aws_vpc.tf_vpc.id
+  cidr_block = var.pri_sub_cidr
+  availability_zone = var.Az[0]
+
+  tags = {
+    Name = "tf_private_subnet"
+  }
+}
+
+
+resource "aws_subnet" "pri_sub_2" {
+  vpc_id = aws_vpc.tf_vpc.id
+  cidr_block = var.pri_sub_cidr
+  availability_zone = var.Az[1]
   
+
+  tags = {
+    Name = "tf_private_subnet"
+  }
 }
+
+
+  resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.tf_vpc.id
+    tags = {
+      Name = "tf_igw"
+    }
+
+  }
