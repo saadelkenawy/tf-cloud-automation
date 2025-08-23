8a1ea73b0371616770bf1334cf9ccf5e6b3ee836
Saad El-Kenawy <Saad_elkenawy@yahoo.com>
2025-08-24 01:55:35 +0300

    fix variable calling

--- test-code/aws/lb3_variables/main.tf
+++ test-code/aws/lb3_variables/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # create a vpc 
 resource "aws_vpc" "tf_vpc" {
-  cidr_block = var.cidr
+  cidr_block = var.vpc_cidr
   enable_dns_support = var.dns_support
   enable_dns_hostnames = var.dns_hostname
 
@@ -33,6 +33,8 @@ resource "aws_subnet" "pri_sub" {
     Name = "tf_private_subnet"
   }
 }
+
+
 
 
   resource "aws_internet_gateway" "igw" {
