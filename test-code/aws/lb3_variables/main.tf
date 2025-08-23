3d5b3ec967bc3c1a2207f0f74a14a8e4fd09d14f
Saad El-Kenawy <Saad_elkenawy@yahoo.com>
2025-08-24 01:38:11 +0300

    remove 2nd subnet pri and  pub beacuse we apply for on value will casue a confilect

--- test-code/aws/lb3_variables/main.tf
+++ test-code/aws/lb3_variables/main.tf
@@ -24,34 +24,10 @@ resource "aws_subnet" "pub_sub" {
 }
 
 
-resource "aws_subnet" "pub_sub_2" {
-  vpc_id = aws_vpc.tf_vpc.id
-  cidr_block = var.pub_sub_cidr
-  availability_zone = var.Az[1]
-  map_public_ip_on_launch = var.public_ip_on_launch
-
-  tags = {
-    Name = "tf_public_subnet_2"
-  }
-}
-
-
 resource "aws_subnet" "pri_sub" {
   vpc_id = aws_vpc.tf_vpc.id
   cidr_block = var.pri_sub_cidr
   availability_zone = var.Az[0]
-
-  tags = {
-    Name = "tf_private_subnet"
-  }
-}
-
-
-resource "aws_subnet" "pri_sub_2" {
-  vpc_id = aws_vpc.tf_vpc.id
-  cidr_block = var.pri_sub_cidr
-  availability_zone = var.Az[1]
-  
 
   tags = {
     Name = "tf_private_subnet"
