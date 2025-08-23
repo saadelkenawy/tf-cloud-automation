4e76926dbb5ffb422f6b5473d9538225fdb84aee
Saad El-Kenawy <Saad_elkenawy@yahoo.com>
2025-08-24 01:55:58 +0300

    assign values to variables.tf

--- test-code/aws/lb3_variables/variables.tf
+++ test-code/aws/lb3_variables/variables.tf
@@ -1,6 +1,7 @@
 variable "aws_region" {
   description = "default region is us-east-1"
   type = string
+  default = "us-east-1"
 }
 
 variable "regions" {
@@ -8,40 +9,41 @@ variable "regions" {
   default = ["us-east-1", "us-west-2"]
 }
 
-variable "cidr" {
+variable "vpc_cidr" {
   description = "user input for cidr applied new resource"
   type = string
+  default = "10.0.0.0/16"
+
 
 }
 
 variable "pub_sub_cidr" {
   description = "user input for public subnet cidr applied new resource"
   type = string
+  default = "10.0.100.0/24"
 
 }
 
 variable "pri_sub_cidr" {
   description = "user input for private subnet cidr applied new resource"
   type = string
+  default = "10.0.1.0/24"
 
 }
 variable "dns_support" {
     description = "select dns support true/false"
     type = bool
+    default = false
 }
 
 variable "dns_hostname" {
     description = "select dns hostname true/false"
     type = bool
-}
-
-variable "vpc_name" {
-  description = "Name of the VPC"
-  type        = string
+    default = false
 }
 
 variable "Az" {
-    description = "availability zone"
+    description = "availability zone List : us-east-1a, us-east-1b, us-east-1c, us-east-1d, us-east-1e , us-east-1f"
     type        = list(string)
     default = [ "us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e" , "us-east-1f" ]
 }
@@ -49,4 +51,5 @@ variable "Az" {
 variable "public_ip_on_launch" {
   description = "Allocate a public IP address on instance launch"
   type        = bool
+  default = false
 }
