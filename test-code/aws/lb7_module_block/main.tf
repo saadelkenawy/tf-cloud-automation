dc2915011f60be04ed43b9d1a212ac246d024211
Saad El-Kenawy <Saad_elkenawy@yahoo.com>
2025-08-27 02:00:57 +0300

    inital intro for module block and output

--- test-code/aws/lb7_module_block/main.tf
+++ test-code/aws/lb7_module_block/main.tf
@@ -1,76 +1,20 @@
-provider "aws" {
-  region = var.aws_region
+ module "subnet_addrs" {
+  source  = "hashicorp/subnets/cidr"
+  version = "1.0.0"
+  
+  base_cidr_block = "10.0.0.0/22"
+  networks = [
+  {
+    name     = "module_network_a"
+    new_bits = 2
+  },
+  {
+    name     = "module_network_b"
+    new_bits = 2
+  },
+ ]
 }
 
-data "aws_region" "current" {
+output "subnet_addrs" {
+  value = module.subnet_addrs.network_cidr_blocks
 }
-data "aws_availability_zones" "available_az" {
-  state = "available"
-}
-
-locals {
-  timestamp = timestamp()
-  vpc_name  = "${var.def_prod}_tfvpc"
-  remote_user = "${var.def_remote_user}_User"
-  subnet_pri = "${var.def_pri}_subnet"
-  subnet_pub = "${var.def_pub}_subnet"
-
-}
-
-# create a vpc 
-resource "aws_vpc" "tf_vpc" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = var.dns_support
-  enable_dns_hostnames = var.dns_hostname
-
-  tags = {
-    Name = local.vpc_name
-    User = local.remote_user
-    Region_saad = data.aws_region.current.region
-
-  }
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id = aws_vpc.tf_vpc.id
-  for_each = var.private_subnets
-  cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value)
-  availability_zone = tolist(data.aws_availability_zones.available_az.names)[each.value]
-  tags = {
-    Name = each.key
-    User = local.remote_user
-  }
-}
-
-# resource "aws_subnet" "pub_sub" {
-#   vpc_id = aws_vpc.tf_vpc.id
-#   cidr_block = var.pub_sub_cidr
-#   availability_zone = var.Az[0]
-#   map_public_ip_on_launch = var.public_ip_on_launch
-
-#   tags = {
-#     Name = local.subnet_pub
-#   }
-# }
-
-
-# resource "aws_subnet" "pri_sub" {
-#   vpc_id = aws_vpc.tf_vpc.id
-#   cidr_block = var.pri_sub_cidr
-#   availability_zone = var.Az[0]
-
-#   tags = {
-#     Name = local.subnet_pri
-#   }
-# }
-
-
-
-
-#   resource "aws_internet_gateway" "igw" {
-#     vpc_id = aws_vpc.tf_vpc.id
-#     tags = {
-#       Name = "tf_igw"
-#     }
-
-#   }
